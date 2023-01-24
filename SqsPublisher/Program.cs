d575c1d1d50254e996e60ad8f59665f66498706a
Diogo Araújo <dg.borges1@gmail.com>
2023-01-24 20:44:32 +0000

    Created an AWS Message Publisher project.

---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "dg.borges1@gmail.com",
+    FullName = "Diogo Araugo",
+    DateOfBirth = new DateTime(1998, 03, 09),
+    GithubUsername = "dba98"
+};
+
+var quereUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = quereUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine(response);
